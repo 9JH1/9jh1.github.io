9b3058cc496fea75a122b8bf166d3e0a7b36bb54
3HY <147371934+9JH1@users.noreply.github.com>
2024-03-12 14:50:39 +1300

    nig

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,7 +1,6 @@
 window.onload = async () => {
     const loader = document.getElementById("loader");
     const loader_page = document.getElementsByClassName("loader")[0];
-    animateTextPar("animated-text-desc-one", 20);
     const list_ = ["---------------------<br>loading webpage", "loading fonts", "loading animations", "loading css", "loading images", "loading some other cool things"];
     const loop_ = new Promise((resolve, reject) => {
         for (let a = 0; a < list_.length; a++) {
@@ -31,10 +30,9 @@ window.onload = async () => {
                 animate("lar-2", 20);
                 setTimeout(() => {
                     document.getElementById("navbar").style.transform = "translateY(0%)";
-                    animate("animate-cent-tag",20)
                     document.body.style.overflowY = "scroll";
-                    document.getElementById("scrollbar").style.transform = "translateX(0%)";
-                    animate_moving_text()
+                    //document.getElementById("scrollbar").style.transform = "translateX(0%)"; 
+                    // NUH UH
                 }, 500);
             }, 200);
         }, 300);
@@ -84,15 +82,49 @@ function animateTextPar(element, duration) {
         }
     }
 }
-
-function animate_moving_text(){
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                animate("moving-text",20)
-                observer.unobserve(document.getElementById("moving-text"));
+document.addEventListener("DOMContentLoaded", () => {
+    animateInTitle("tester")
+})
+function animateInTitle(newTitle) {
+    newTitle = newTitle.toUpperCase
+    const list_ = String(newTitle).split('');
+    const title_ = document.getElementById("title-act");
+    const old_ = title_.innerText;
+    const char_ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%&".split("");
+    let render_ = [];
+    let titleText = old_.split('');
+    for (let i = 0; i < old_.length; i++) {
+        let randomNumber = Math.round(Math.random() * (30 - 6) + 6);
+        for (let ii = 0; ii < randomNumber; ii++) {
+            let randomNumberChar = Math.round(Math.random() * (char_.length - 0));
+            titleText[i] = char_[randomNumberChar];
+            console.log(titleText);
+            render_.push(titleText);
+        }
+    }
+    console.log(render_);
+    function RenderTitle() {
+        for (let iii = 0; iii < render_.length; iii++) {
+            let titleSample = "";
+            for (let iiii = 0; iiii < render_[iii].length; iiii++) {
+                titleSample = titleSample + render[iii][iiii];
+                setTimeout(() => {
+                    title_.innerText = titleSample;
+                }, (700 * iii) * iiii);
             }
-        });
-    });
-    observer.observe(document.getElementById("moving-text"));
+        }
+    }
 }
+
+
+
+/*
+const letter_ = list_[i];
+const randomI_ = Math.random * 100;
+
+setTimeout(() => {
+    title_.innerText += letter_;
+}, (700 * i) + 500);
+console.log(letter_)
+console.log(i);
+*/
