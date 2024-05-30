b84c79653e2d7f7983ff6f36dbe96d3778f2e57d
9JH1 <tkf.x1os@gmail.com>
2024-05-30 14:22:28 +1200

    fix

--- src/js/datetime.js
+++ src/js/datetime.js
@@ -7,13 +7,9 @@ function setDateTime() {
         hour12: true, 
     });
     document.getElementById("time").innerHTML = String(document.getElementById("time").innerHTML).replace(":", "<span class='blink'>:</span>");
-
-    document.getElementById("date").innerHTML = `${event.clientX}x-${event.clientY}y`
 }
 
 setDateTime();
-
-
 
 setInterval(() => {
     setDateTime();
