bafbd6552cc58981cdee13dfac9d07a206d50268
9JH1 <tkf.x1os@gmail.com>
2024-05-31 16:00:35 +1200

    cleanup

--- src/js/projects.js
+++
@@ -1,6 +0,0 @@
-if(document.body.getBoundingClientRect().width < 1120){ 
-    window.addEventListener("resize",()=>{
-        const projects = document.getElementById("projects"); 
-        projects.style.bottom = document.body.getBoundingClientRect().height + "px"
-    })
-}
