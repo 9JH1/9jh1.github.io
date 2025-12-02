13a28b82b5e20b13da3ae7326b60f5a312d3251f
9jh1 <tkf.x1os@gmail.com>
2025-12-02 20:11:03 +1300

    fixed time zones?

--- public.js
+++ public.js
@@ -1,10 +1,11 @@
 // Log start time 
 const st = performance.now();
 
-function format_time(ms)
-{
+function format_time(date) {
+	let ms = Date.now() - date.getTime();
+	console.log(ms);
 	if (!ms || ms <= 0) return "0s ago";
-	ms = Math.floor(ms / 1000); // now in seconds
+	ms = Math.floor(ms / 1000); 
 
 	const units = [
 		{ label: "d", secs: 86400 },
@@ -20,45 +21,35 @@ function format_time(ms)
 			str += `${val}${label} `;
 			ms %= secs;
 		}
-		if (str) break; // stop after first non-zero
+		if (str) break; 
 	}
 
 	return (str || "0s").trim() + " ago";
 }
 
 // Initialize on page load
-window.onload = function() 
-{
+window.onload = function() {
 	const title = document.getElementById("title");
 	const roota = document.documentElement;
 	const rootw = document.getElementById("base");
 	const wcont = document.getElementById("content");
-	let posx, posy;
 
 	// Initialize page
 	const unit = rootw.getBoundingClientRect();
 	const default_title = "Home"
+
+	title.innerText = default_title;
 	rootw.classList.remove("startup");
 	rootw.classList.add("root");
+	rootw.innerHTML = "";
 
 	// Set styles
 	roota.style.setProperty("--unit-w", unit.width + "px");
 	roota.style.setProperty("--unit-h", unit.height + "px");
 
-	// Update positions
-	window.addEventListener("mousemove", (e) => {
-		posx = Math.floor(e.clientX / unit.width);
-		posy = Math.floor(e.clientY / unit.height);
-	});
-
 	// Set dimensions of root element
-	function resize_root_element() 
-	{
-		const u_w = (Math.floor(window.innerWidth / unit.width) - 1) * unit.width;
-		/* const u_h = (Math.floor(window.innerHeight / unit.height) - 1) * unit.height; */
-
-		rootw.style.width = u_w + "px";
-		/*rootw.style.height = u_h + "px";*/
+	function resize_root_element() {
+		rootw.style.width = (Math.floor(window.innerWidth / unit.width) - 1) * unit.width + "px"
 	}
 
 	resize_root_element();
@@ -69,6 +60,8 @@ window.onload = function()
 	wcont.innerHTML.split('\n').forEach((line) => out += line.trimStart());
 	rootw.innerHTML = out;
 	wcont.remove();
+
+	// Load json data
 	const projw = document.getElementById("projects");
 	const jourw = document.getElementById("journal");
 
@@ -81,21 +74,20 @@ window.onload = function()
 		.then(data => {
 			for (const project of data["projects"]) {
 				const h_item = document.createElement("div");
+				const h_item_desc_ul = document.createElement("ul");
+				const h_item_desc_div = document.createElement("div");
+
 				h_item.classList.add("li");
 				h_item.innerHTML += `<p><a href="${project.url}">${project.name}</a></p>`
 				h_item.innerHTML += `<span class="l-text" title="description | tag(s)">${project.tagline} | ${project.tags.join(", ")}</span>`
-
-				const h_item_desc_div = document.createElement("div");
 				h_item.append(h_item_desc_div);
-
-				const h_item_desc_ul = document.createElement("ul");
 				h_item_desc_div.append(h_item_desc_ul);
+				projw.append(h_item);
 
 				for (const desc of project["description"]) {
 					h_item_desc_ul.innerHTML += `<li>${desc.cont}</li>`
 				}
 
-				projw.append(h_item);
 
 				h_item.addEventListener("mouseenter", () => {
 					title.innerText = `${project.name} | ${project.tagline}`
@@ -109,29 +101,31 @@ window.onload = function()
 
 			for (const entry of data["journal"]) {
 				const h_item = document.createElement("div");
-				const date = new Date(entry.date);
-				const date_s = entry.date.replace(/-/g,'/').replace('T', ' ').split('.')[0];
 				const h_cont = document.createElement("div");
-				const now = new Date();
-				const time_since = format_time(now - date);
+				const date = new Date(entry.date);
+				const date_s = date.toLocaleDateString(undefined, {
+					year: 'numeric',
+					month: 'numeric',
+					day: 'numeric'
+				}) + " " + date.toLocaleTimeString(undefined, {
+					hour: '2-digit',
+					minute: '2-digit',
+					second: '2-digit',
+					hour12: false
+				});
 
 				h_item.classList.add("li");
 				h_item.innerHTML += `<p><b>${entry.title}</b></p>`;
-				h_item.innerHTML += `<span class="l-text" title="${date_s}">${time_since} | ${entry.tags.join(", ")}</span>`;
+				h_item.innerHTML += `<span class="l-text" title="Posted ${date_s}">${format_time(date)} | ${entry.tags.join(", ")}</span>`;
+				h_cont.innerText = entry.content;
 
 				h_item.append(h_cont);
-				h_cont.innerText = entry.content;
 				jourw.append(h_item);
 			}
 		})
-		.catch(err => {
-			console.error("Loading data.json threw error: " + err);
-		})
 
 	// Scroll to top of document
-	if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
-	}
+	if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
 
 	// Log load-time 
