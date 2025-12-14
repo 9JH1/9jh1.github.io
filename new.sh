d3a8a64f53a542926f6c5bfd0212d3bfbf6194c5
Lucy Adams <tkf.x1os@gmail.com>
2025-12-14 23:11:28 +1300

    new data

--- new.sh
+++ new.sh
@@ -8,19 +8,22 @@ read -p "Enter Tags: " tags
 tmp=$(mktemp --suffix=".txt")
 nvim "$tmp"
 
-# Get json var
-read -r -d '' JSON << EOM 
-{
-	"title": "$title",
-	"date": "$(date -u '+%Y-%m-%dT%H:%M:%SZ')",
-	"content": "$(cat "$tmp")"
-}
-EOM
+# Create new json object
+NEW_JSON="$(jq -n \
+	--arg title "$title" \
+	--arg date "$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
+	--rawfile content "$tmp" \
+	'{
+		title: $title,
+    	date: $date,
 
-# Write new json and push new data
-jq --argjson d "$JSON" '.journal+=[$d]' src/data.json > "src/data.json"
+    	content: $content
+	}')"
 
-# Generate new index file 
+# Push to data file 
+jq --argjson d "$NEW_JSON" '.journal+=[$d]' src/data.json
+
+#Generate new index file 
 cd build
 ./gen.sh 
 cd ..
@@ -30,6 +33,5 @@ git add src/data.json
 git add index.html
 
 git commit -m "added new journal entry"
-git push
 
-echo "Done!"
+
